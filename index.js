ca9263f2faaaae0ae8dfe376349d71cacdae0a78
Gloria Lim <glorialimartt@gmail.com>
2024-06-19 23:31:34 -0400

    Installed puppeteer. Experiment with scraping data from a new page that opens after clicking a button.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // @ts-nocheck
 const axios = require('axios');
 const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 
 // URL of website to scrape
 const url = 'https://boxingontario.com/clubs/club-directory/'
@@ -25,8 +26,8 @@ async function scrapeData() {
   const $ = cheerio.load(html);
 
   // Extract title of page
-  const title = $('head > title').text();
-  console.log(`Title: ${title}`);
+  // const title = $('head > title').text();
+  // console.log(`Title: ${title}`);
 
   // Extract all links on the page
   // $('a').each((index, element) => {
@@ -41,7 +42,53 @@ async function scrapeData() {
     const club = $(element).text();
     console.log(club)
   })
+  // $('.club-title').each((index, element) => {
+    
+  // })
+  const urlTest = $('a[class=view-club]').attr('href');
+  console.log(urlTest)
+
 }
 
 // Invoke the scraper function
-scrapeData();
+// scrapeData();
+
+
+async function testFetch(url) {
+  try {
+
+    // Launch puppeteer
+    const browser = await puppeteer.launch({headless: false });
+    const page = await browser.newPage();
+  
+    // Go to the initial page
+    await page.goto(url);
+  
+    // Wait for the button then click it
+    await page.waitForSelector('.view-club')
+    await page.click('.view-club');
+
+    // Wait for the new page/tab to open
+    const newPagePromise = new Promise(x => browser.once('targetcreated', target => x(target.page())));
+    const newPage = await newPagePromise;
+
+    await newPage.waitForSelector('.club-info');
+
+    // Get the HTML content of the page
+    const content = await newPage.content();
+    
+    // Load the HTML into Cheerio
+    const $ = cheerio.load(content);
+  
+    // Scrape data from the new page
+    const data = $('.club-info').html();
+    console.log(data);
+
+    // Close the browser
+    await browser.close();  
+  } catch (error) {
+    console.error(`Error fetching the URL: ${error.message}`)
+  }
+}
+
+testFetch();
